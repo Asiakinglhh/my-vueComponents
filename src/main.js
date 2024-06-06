d1b3e3210b7fbe174c95a4161cc5c57546653e7a
Asiakinglhh <751952566@qq.com>
2024-06-06 15:40:33 +0800

    add datav library

--- src/main.js
+++ src/main.js
@@ -2,10 +2,13 @@ import Vue from 'vue'
 import './element'
 import directives from './directives'
 import './components/Graph/register'
+import { waterLevelPond, scrollBoard } from '@jiaminghi/data-view'
 import App from './App'
 import router from './router'
 
 Vue.use(directives)
+Vue.use(waterLevelPond)
+Vue.use(scrollBoard)
 
 Vue.config.productionTip = false
 
